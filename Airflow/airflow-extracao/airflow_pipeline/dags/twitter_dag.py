365f1b83870373015dfb6e2ff55d1ed917e2797e
Kennedy M <menezes_kennedy@hotmail.com>
2024-10-15 17:03:26 -0300

    arrumando pasta

--- Airflow/airflow-extracao/airflow_pipeline/dags/twitter_dag.py
+++ Airflow/airflow-extracao/airflow_pipeline/dags/twitter_dag.py
@@ -7,14 +7,16 @@ from operators.twitter_operator import TwitterOperator
 from os.path import join
 from airflow.utils.dates import days_ago
 
+with DAG(dag_id = "TwitterDAG", start_date=days_ago(2), schedule_interval="@daily") as dag:
 
-with DAG(dag_id = "TwitterDag", start_date=days_ago(2), schedule_interval="@daily") as dag:
-
-    TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.00Z'
+    TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
     query = "datascience"
-        
-    to = TwitterOperator(
-        file_path = join("datalake/twitter_datascience", 
-                                            "extract_date={{ ds }}",
-                                            "datascience_{{ ds_nodash }}.json"), 
-                                            query=query, start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", end_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", task_id="twitter_datascience")
+
+    to = TwitterOperator(file_path=join("datalake/twitter_datascience",
+                                        "extract_date={{ ds }}",
+                                        "datascience_{{ ds_nodash }}.json"),
+                                        query=query,
+                                        start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
+                                        end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
+                                        task_id="twitter_datascience")
+    
--- Airflow/airflow-extracao/airflow_pipeline/operators/twitter_operator.py
+++ Airflow/airflow-extracao/airflow_pipeline/operators/twitter_operator.py
@@ -12,18 +12,17 @@ class TwitterOperator(BaseOperator):
 
     template_fields = ["query", "file_path", "start_time", "end_time"]
 
-    def __init__(self, file_path, end_time, start_time, query, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self,file_path, end_time, start_time, query, **kwargs):
         self.end_time = end_time
         self.start_time = start_time
         self.query = query
         self.file_path = file_path
+        super().__init__(**kwargs)
 
     def create_parent_folder(self):
         (Path(self.file_path).parent).mkdir(parents=True, exist_ok=True)
-
+    
     def execute(self, context):
-
         end_time = self.end_time
         start_time = self.start_time
         query = self.query
@@ -36,19 +35,17 @@ class TwitterOperator(BaseOperator):
                 output_file.write("\n")
 
 if __name__ == "__main__":
-
-    TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.00Z'
+    #montando url
+    TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
 
     end_time = datetime.now().strftime(TIMESTAMP_FORMAT)
     start_time = (datetime.now() + timedelta(-1)).date().strftime(TIMESTAMP_FORMAT)
-    query = "data science"
+    query = "datascience"
 
     with DAG(dag_id = "TwitterTest", start_date=datetime.now()) as dag:
-        
-        to = TwitterOperator(file_path = join("datalake/twitter_datascience", 
-                                              f"extract_date={datetime.now().date()}",
-                                              f"datascience_{datetime.now().date().strftime('%Y%m%d')}.json"), 
-                                              query=query, start_time=start_time, end_time=end_time, task_id="test_run")
+        to = TwitterOperator(file_path=join("datalake/twitter_datascience",
+                                            f"extract_date={datetime.now().date()}",
+                                            f"datascience_{datetime.now().date().strftime('%Y%m%d')}.json"),
+                                            query=query, start_time=start_time, end_time=end_time, task_id="test_run")
         ti = TaskInstance(task=to)
         to.execute(ti.task_id)
-
